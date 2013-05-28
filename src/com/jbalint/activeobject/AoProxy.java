b810b386a7a021bea2b52af73b6346d73d2ca3ff
Jess Balint <jbalint@gmail.com>
2013-05-27 21:31:09 -0500

    small improvements

--- src/com/jbalint/activeobject/AoProxy.java
+++ src/com/jbalint/activeobject/AoProxy.java
@@ -2,6 +2,7 @@ package com.jbalint.activeobject;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 public class AoProxy implements InvocationHandler {
     private Object target;
@@ -16,5 +17,10 @@ public class AoProxy implements InvocationHandler {
 	MethodRequest mr = new MethodRequest(target, method, args);
 	scheduler.insert(mr);
 	return null; // TODO
+    }
+
+    public static Object create(Object servant, Scheduler scheduler, Class interfaces[]) {
+	AoProxy aoProxy = new AoProxy(servant, scheduler);
+	return Proxy.newProxyInstance(AoProxy.class.getClassLoader(), interfaces, aoProxy);
     }
 }
--- src/com/jbalint/activeobject/DemoApp.java
+++ src/com/jbalint/activeobject/DemoApp.java
@@ -1,15 +1,11 @@
 package com.jbalint.activeobject;
 
-import java.lang.reflect.*;
-import java.util.*;
-
 public class DemoApp {
     public static void main(String args[]) throws InterruptedException /* sleep() */ {
 	DemoMessageQueue demoMq = new DemoMessageQueue();
 	Scheduler scheduler = new Scheduler();
 	scheduler.runService();
-	AoProxy proxy = new AoProxy(demoMq, scheduler);
-	MessageQueue mqProxy = (MessageQueue)Proxy.newProxyInstance(DemoApp.class.getClassLoader(), new Class[] {MessageQueue.class}, proxy);
+	MessageQueue mqProxy = (MessageQueue)AoProxy.create(demoMq, scheduler, new Class[] {MessageQueue.class});
 	mqProxy.put("test");
 	Thread.sleep(1000);
     }
